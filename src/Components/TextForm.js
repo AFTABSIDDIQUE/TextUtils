382020f3b4d565482194b281504770bb019d5dec
AFTABSIDDIQUE <aftabsiddique2526@gmail.com>
2024-11-21 12:28:39 +0530

    third commit

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -41,20 +41,20 @@ export default function TextForm(props) {
        <div className='container my-3' style={{color: props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
        <div className="mb-3">
-            <textarea className="form-control" id="myBox" style={{backgroundColor: props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}  rows="8" placeholder='Enter the Text in TextBox' value={text} onChange={handleOnClick} ></textarea>
+            <textarea className="form-control" id="myBox" style={{backgroundColor: props.mode==='dark'?'lightgrey':'white',color:props.mode==='dark'?'white':'black'}}  rows="8" placeholder='Enter the Text in TextBox' value={text} onChange={handleOnClick} ></textarea>
         </div>
-        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
-        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to LowerCase</button>
-        <button className="btn btn-primary mx-1" onClick={handleRemoveSpaces}>Remove Extra Spaces</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary mx-1" onClick={handleClear}>Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to LowerCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleRemoveSpaces}>Remove Extra Spaces</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClear}>Clear</button>
         </div>
         <div className='container my-3'style={{color: props.mode==='dark'?'white':'black'}}>
             <h2>Text Summary</h2>
-            <p>{text.split(" ").length-1} words and {text.length} characters </p>   
-            <p>{0.008*text.split(" ").length} Minutes to Read a Word </p>
+            <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters </p>   
+            <p>{0.008*text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes to Read a Word </p>
             <h2>Preview</h2>
-            <p>{text.length>0?text:'Please Enter the text into the above textarea to see the text'}</p>        
+            <p>{text.length>0?text:'Nothing to preview'}</p>        
         </div>       
     </>
   )
